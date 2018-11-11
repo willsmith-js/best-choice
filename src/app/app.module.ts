f9102910f3f35d1e228297847ea7954d8b56604e
Gurgen <gurgentorosyan88@yandex.ru>
2018-11-11 16:51:23 +0400

    implamanted ngx-translate package, welcome page was translated, 3 flag icons was added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
-import {NgModule} from '@angular/core';
-import {MomentModule} from 'ngx-moment';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MomentModule } from 'ngx-moment';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {HeaderComponent} from './components/header/header.component';
-import {WelcomeComponent} from './pages/welcome/welcome.component';
-import {AppComponent} from './components/root/app.component';
-import {HomeComponent} from './pages/home/home.component';
-import {AppRoutingModule} from './app-routing.module';
-import {CoreModule} from './core/core.module';
-import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { AppComponent } from './components/root/app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module'
+import { CoreModule } from './core/core.module';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -24,7 +32,16 @@ import { ContactUsComponent } from './pages/contact-us/contact-us.component';
     AppRoutingModule,
     BrowserModule,
     MomentModule,
-    CoreModule
+    CoreModule,
+    SharedModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
