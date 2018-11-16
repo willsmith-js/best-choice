1a5a251d64959bd1d06bf4af30f8ab4f1178b4d3
FilipVirabyan <42550377+FilipVirabyan@users.noreply.github.com>
2018-11-16 18:13:04 +0400

    Update sport-school-routing.module.ts

--- src/app/pages/sport-school/sport-school-routing.module.ts
+++ src/app/pages/sport-school/sport-school-routing.module.ts
@@ -3,7 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import {SportSchoolComponent} from "./components/sport-school.component";
 
 const routes: Routes = [
-    { path: "", component:SportSchoolComponent }
+    { 
+      path: "", 
+      component:SportSchoolComponent 
+    }
 ];
 
 @NgModule({
